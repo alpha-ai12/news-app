84400584c30af6028aeb86fa7b530be219f3a3c5
CodeSourabh <sourabh.gupta@ultra.me>
2024-08-16 17:31:53 +0530

    changes for node 16

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CompressionPlugin = require("compression-webpack-plugin");
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
+  config.optimization.usedExports = true;
   if (env.mode === "production") {
     config.plugins.push(
       new CompressionPlugin({
