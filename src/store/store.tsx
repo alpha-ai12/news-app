348c2e753f903cd6fcc97985fb43aab58958692b
CodeSourabh <sourabh.gupta@ultra.me>
2024-08-23 13:51:51 +0530

    update APi Url

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -10,7 +10,7 @@ import Code from "../screens/Board/partials/country/code.json";
 export const STORE_FEATURE_KEY = "store";
 export const storeAdapter = createEntityAdapter();
 const API_URL =
-  "https://news-backend-eosin.vercel.app/news";
+  "news-backend-15lhs6kji-codeninjas-projects.vercel.app/news";
 export const getCountryNameFromId = (id: string) => {
   let name = "";
   Code.forEach((i) => {
